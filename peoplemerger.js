68cf928b44882c2b51c58c39f5777a5c49c5bf9d
Ravi Panchal <pancha.ravi@gmail.com>
2017-04-20 20:29:37 -0400

    corrected the Logic

--- peoplemerger.js
+++ peoplemerger.js
@@ -7,29 +7,23 @@ fs.readFile('./people1.JSON', 'utf-8', function(err, data){
 		console.log(`an error occurred reading file 1: ${err}`);        
   	 	throw err;  
   	 	    } else {
-  	 allpeople1 = JSON.parse(data);
+  	  allpeople1 = JSON.parse(data);
+      fs.readFile('./people2.JSON', 'utf-8', function(err, data){    
+      if (err) {                 
+      console.log(`an error occurred reading file 2: ${err}`);        
+        throw err;  
+          } else {
+      allpeople2 = JSON.parse(data);
+      mergeArray(allpeople2,allpeople1);
+      allpeople2.sort(ascending);
+      fs.writeFile('./peopleComplete.txt', allpeople2, (err) => {
+        if (err) throw err;
+        console.log('New file has been created');
+      });
+    }
+  });
   	}
 });
 
 
-
-fs.readFile('./people2.JSON', 'utf-8', function(err, data){    
-  if (err) {                 
-    console.log(`an error occurred reading file 2: ${err}`);        
-      throw err;  
-        } else {
-    allpeople2 = JSON.parse(data);
-     
-    mergeArray(allpeople2,allpeople1);
-  
-    allpeople2.sort(ascending);
-    fs.writeFile('./peopleComplete.txt', allpeople2, (err) => {
-      if (err) throw err;
-      console.log('New file has been created');
-    });
-
-    for (var i =0 ;i < allpeople2.length;i++)
-    console.log(allpeople2[i]);
-  }
-});
 
